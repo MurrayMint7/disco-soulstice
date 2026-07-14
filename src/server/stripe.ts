e130d5dfaf768681561380dfa8b48e6d1edcd031
Josh Murray <joshuaamurray22@gmail.com>
2026-07-14 17:23:34 +0100

    made stripe lazy instantiate

--- src/server/stripe.ts
+++ src/server/stripe.ts
@@ -1,7 +1,18 @@
 import Stripe from "stripe";
 import { env } from "~/env";
 
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2026-02-25.clover",
-  typescript: true,
+let _stripe: Stripe | undefined;
+
+function getStripe() {
+  _stripe ??= new Stripe(env.STRIPE_SECRET_KEY, {
+    apiVersion: "2026-02-25.clover",
+    typescript: true,
+  });
+  return _stripe;
+}
+
+export const stripe = new Proxy({} as Stripe, {
+  get(_target, prop, receiver) {
+    return Reflect.get(getStripe(), prop, receiver) as unknown;
+  },
 });
